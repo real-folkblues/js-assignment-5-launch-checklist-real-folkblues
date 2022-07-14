d39c5ae776cb5bb20fbebd769150996bdfa2e424
Real-folkblues <marenicolor@gmail.com>
2022-07-14 14:25:44 -0400

    Req 2.a

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// Write your JavaScript code here!
-
 window.addEventListener("load", function() {
+    // document.getElementById("launchstatus").addEventListener("click", function (event) {
+    //     event.preventDefault()
+    // });
+    let form = document.querySelector("launchForm");
+    form.addEventListener("submit", function (event) {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("All fields are required!");
+        }
+    });
 
    let listedPlanets = myFetch();
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
